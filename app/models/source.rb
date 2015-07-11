2e3f686e26a015a501dd2062388b618d9808452f
Guillaume Illien <guillaume.illien@gmail.com>
2015-07-11 15:59:27 +0800

    Fixed attachment issue

--- app/models/source.rb
+++ app/models/source.rb
@@ -3,8 +3,9 @@ class Source < ActiveRecord::Base
 
   has_many :articles
 
-  has_attached_file :logo, styles: { thumb: "50x50>" }, default_url: 'http://placehold.it/50x50'
-
+  has_attached_file :logo, styles: { thumb: '50x50>' }, default_url: 'http://placehold.it/50x50'
+  
+  validates_attachment_content_type :logo, content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
   validates :description, presence: true
   validates :key, presence: true, uniqueness: true
   validates :uri, presence: true, format: URI::regexp(%w(http https))
